dd0972dae5fd2a21ea3c0377060bc35f7fd09090
PedroAguirre23 <paguirre@fi.uba.ar>
2021-11-26 21:35:02 -0300

    objetivo cansado

--- cansado.cpp
+++ cansado.cpp
@@ -12,3 +12,15 @@ Cansado::Cansado(string objetivo, string tipo) : Objetivo(objetivo, tipo)
 Cansado::~Cansado()
 {
 }
+
+bool Cansado::chequear_estado(int puntos_energia){
+    bool aux = false;
+
+    if(puntos_energia == CANTIDAD_MIN_ENERGIA){
+        aux = true;
+        objetivo_cumplido = true;
+    }
+
+    return aux;
+}
+
