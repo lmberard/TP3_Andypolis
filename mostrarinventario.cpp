a3488663fbf426010d2a3cc32efa20a8b863e5ac
lmberard <lberard@fi.uba.ar>
2021-12-08 22:40:48 -0300

    para probar de cero

--- mostrarinventario.cpp
+++
@@ -1,7 +0,0 @@
-#include "mostrarinventario.hpp"
-
-
-void MostrarInventario::jugar(Constructor & bob, Mapa & mapa, int & turno, Jugador * jugador, int & id_jugador_actual)
-{
-    jugador[id_jugador_actual-1].mostrar_inventario();
-}
--- mostrarmapa.cpp
+++
@@ -1,6 +0,0 @@
-#include "mostrarmapa.hpp"
-
-void MostrarMapa::jugar(Constructor & bob, Mapa & mapa, int & turno, Jugador * jugador, int & id_jugador_actual)
-{
-    mapa.mostrar();
-}
