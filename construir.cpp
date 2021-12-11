6a63ca49042312f8d23bd507bf665aaa265d27e2
PedroAguirre23 <paguirre@fi.uba.ar>
2021-12-10 22:01:15 -0300

    Borro comentarios

--- construir.cpp
+++ construir.cpp
@@ -21,7 +21,7 @@ void Construir::jugar(Constructor & bob, Mapa & mapa, int & turno, Jugador * jug
             }   
         }
     }
-    
+
     bool fin_turno = false;
     jugadores[id_jugador_actual-1].chequear_objetivos(fin_turno,bob);     
 }
@@ -51,8 +51,6 @@ bool Construir::chequear_edificios_disponibles(Edificio * edif_ptr, Jugador & ju
 }
 
 
-
-
 bool Construir::chequear_requerimientos_edificio(Edificio * edif_ptr, Jugador & jugador){
     return jugador.inv().chequear_stock(edif_ptr, false);
 }
@@ -61,41 +59,3 @@ bool Construir::descontar_materiales(Edificio * edif_ptr, Jugador & jugador){
     return jugador.inv().chequear_stock(edif_ptr, true);
 }
 
-
-/*
-void buildBuildingByName(List<Materials> &materialsChain, List<BuildingInfo> &buildingsInfoChain, Map &andyMap){
-    int row, column;
-    string building;
-    Node<BuildingInfo> * ptrBuildInfoNode = nullptr;
-
-    cout << TXT_ORANGE_166 << ENTER_BUILDING_TO_BUILD << endl << END_COLOR;
-    getline(cin, building);
-    
-
-    if(searchBuildingByName(buildingsInfoChain, &ptrBuildInfoNode, building) == true){
-        if(checkBuildingRequirements(ptrBuildInfoNode, materialsChain) == true){
-            askCoordinates(row,column);
-            if(andyMap.validateCoordinates(row, column) == true){
-                if((ptrBuildInfoNode->getData()).getBuildingsAllowed() - (ptrBuildInfoNode->getData()).getBuildingsMade() > 0){
-                    if( ((CellBuildable*)(andyMap.getElement(row, column)))->getIsBuilt() != true ){
-                        if(ConfirmationToBuild() == CONFIRMATION_BUILD_YES){
-                            if(andyMap.buildBuilding(row, column, building) == true){
-                                ptrBuildInfoNode->getData().addCoordinates(row, column);
-                                ptrBuildInfoNode->getData().setBuildingsMade(ptrBuildInfoNode->getData().getBuildingsMade() + 1);
-                                updateMaterialsAmount(ptrBuildInfoNode, materialsChain);
-                                cout << TXT_ORANGE_166 << BUILD_BUILDING_SUCCESS << building << END_COLOR << endl;
-                            }
-                        }
-                    }
-                    else
-                        cerr << TXT_DARK_RED_1 << ERR_CELL_WITH_BUILDING << END_COLOR << endl << endl;
-                }
-                else
-                    cerr << TXT_DARK_RED_1 << ERR_NO_REMAINING_BUILDINGS_TO_BUILD << END_COLOR << endl << endl;
-            } else
-                cerr << TXT_DARK_RED_1 <<  ERR_INVALID_COORDINATES << END_COLOR << endl << endl;
-        }
-    } else
-        cerr << TXT_DARK_RED_1 << ERR_BUILDING_NOT_FOUND << END_COLOR<< endl << endl;
-}
-*/
