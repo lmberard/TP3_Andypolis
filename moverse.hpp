649366a93eb863cb6f441aba976caafbd5e2b388
PedroAguirre23 <paguirre@fi.uba.ar>
2021-12-12 17:54:04 -0300

    Opción moverse camino minimo

--- moverse.hpp
+++ moverse.hpp
@@ -8,16 +8,45 @@ class Moverse : public Jugada{
         void jugar(Constructor & bob, Mapa & mapa, int & turno, Jugador * jugador, int & id_jugador_actual){
             
             Coordenada coordenadas;
+            Coordenada coord_aux;
+            Lista<Coordenada> camino_realizado;
+            int costo;
             //Grafo grafo;
+
+            for(int i = 0; i < mapa.obtener_filas(); i++){
+                for (int j = 0; j < mapa.obtener_columnas(); j++)
+                    grafo.agregarVertice(coordenadas);
+            }
 
             for(int i = 0; i < mapa.obtener_filas(); i++){
                 for (int j = 0; j < mapa.obtener_columnas(); j++)
                 {
                     coordenadas = crear_coordenada(i, j);
-                    cout << "(" << coordenadas.coord_x << "," << coordenadas.coord_y << ")" << endl;
-                    //grafo.agregarVertice(coordenadas);
+
+                    if(j + 1 < mapa.obtener_columnas()){
+                        coord_aux.coord_x = i;
+                        coord_aux.coord_y = j + 1;
+                        grafo.agregarCamino(coordenadas, coord_aux, mapa.obtener_peso_del_mapa(jugador_id, coord_aux))
+                    }
+                    if(j - 1 > 0){
+                        coord_aux.coord_x = i;
+                        coord_aux.coord_y = j - 1;
+                        grafo.agregarCamino(coordenadas, coord_aux, mapa.obtener_peso_del_mapa(jugador_id, coord_aux))
+                    }
+                    if(i + 1 < mapa.obtener_filas()){
+                        coord_aux.coord_x = i + 1;
+                        coord_aux.coord_y = j;
+                        grafo.agregarCamino(coordenadas, coord_aux, mapa.obtener_peso_del_mapa(jugador_id, coord_aux))
+                    }
+                    if(i - 1 > 0){
+                        coord_aux.coord_x = i - 1;
+                        coord_aux.coord_y = j;
+                        grafo.agregarCamino(coordenadas, coord_aux, mapa.obtener_peso_del_mapa(jugador_id, coord_aux))
+                    }                    
                 }        
             }
+            // pedir a usuario a donde se quiere mover (identificar donde estamos)
+            // costo = grafo.obtener_camino_minimo(desde, hasta, lista<Ubicaciones>)
 
             bool fin_turno = false;
             jugador[id_jugador_actual-1].chequear_objetivos(fin_turno,bob);
